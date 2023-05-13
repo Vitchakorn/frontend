f9cf5897cca929b090858aa7f624afd9e6b29421
Vitchakorn <112546047+Vitchakorn@users.noreply.github.com>
2023-05-13 10:37:54 +0700

    adding files in frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
+const api = axios.create({
+  
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="book-container">
+        <h2>BeCollec</h2>
+        <div className="book-item">
+
+        </div>
+      </div>
   );
 }
 
